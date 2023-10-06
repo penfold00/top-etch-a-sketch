92f279d5232fa60569c2246abd0f20dc071ca23d
penfold00 <109156188+penfold00@users.noreply.github.com>
2023-10-06 14:22:05 +0200

    added radios to chose mode

--- script.js
+++ script.js
@@ -13,24 +13,53 @@ function createGrid(gridWidth, squaresPerSide) {
     square.style.cssText = `width: ${squareDimensions}px; height: ${squareDimensions}px`;
     square.classList.add("square");
     square.setAttribute("id", `square${i}`);
+    square.style["background-color"] = "rgb(240,240,60)";
     gridContainer.appendChild(square);
     addHoverListener(square);
   }
 }
-createGrid(600, 100);
+createGrid(600, 16);
+let mode = "simple";
 
 function addHoverListener(element) {
   element.addEventListener("mouseover", () => {
-    hoverEffect(element);
-    console.log(element);
+    hoverEffect(element, mode);
   });
 }
 
-function hoverEffect(element) {
-  element.classList.add("activated");
+function hoverEffect(element, mode) {
+  if (mode == "simple") {
+    element.style["background-color"] = "rgb(255,255,255)";
+  }
+
+  if (mode == "rainbow") {
+    element.style["background-color"] = `rgb(${getRandomNumber(
+      256
+    )}, ${getRandomNumber(256)}, ${getRandomNumber(256)})`;
+  }
+
+  if (mode == "blacken") {
+    rgbValue = element.style["background-color"].slice(4, -1);
+    let rgbArray = rgbValue.split(",");
+    console.log(rgbArray);
+    element.style["background-color"] = `rgb(${rgbArray[0] - 20},${
+      rgbArray[0] - 20
+    },${rgbArray[0] - 20})`;
+  }
+}
+
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
 }
 
 const newGridButton = document.querySelector("#new-grid");
 newGridButton.addEventListener("click", () => {
   createGrid(600, parseInt(prompt("Squares per side?")));
 });
+
+const radiosMode = document.querySelectorAll("input[name='mode']");
+for (let i = 0; i < radiosMode.length; i++) {
+  radiosMode[i].addEventListener("click", () => {
+    mode = radiosMode[i].value;
+  });
+}
